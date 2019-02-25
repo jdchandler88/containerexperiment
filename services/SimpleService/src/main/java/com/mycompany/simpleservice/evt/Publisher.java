7605ce973f5b8ae26b8ffe6f1a5ef0490d94d0a5
jdchandler88 <jdchandler88@gmail.com>
2019-02-25 15:14:35 -0500

    initial code to move connection and lookup information in simple service to props files

--- services/SimpleService/src/main/java/com/mycompany/simpleservice/evt/Publisher.java
+++ services/SimpleService/src/main/java/com/mycompany/simpleservice/evt/Publisher.java
@@ -2,10 +2,10 @@ package com.mycompany.simpleservice.evt;
 
 import java.util.Properties;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
-import javax.jms.Destination;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -15,20 +15,45 @@ import javax.naming.InitialContext;
 @Stateless
 public class Publisher {
 
+	private String initialContextFactory;
+
+	private String initialContextLookupProtocol;
+	
+	private String jmsHost;
+	
+	private String jmsPort;
+	
+	private String remoteConnectionFactoryJndiName;
+	
+	private String topicJndiName;
+	
+	private String providerUrl;
+	
+	@PostConstruct
+	public void init() {
+		initialContextFactory = System.getProperty("INITIAL_CONTEXT_FACTORY", "\"org.jboss.naming.remote.client.InitialContextFactory");
+		initialContextLookupProtocol = System.getProperty("CONTEXT_LOOKUP_PROTOCOL", "http-remoting");
+		jmsHost = System.getProperty("JMS.HOST", "jms");
+		jmsPort = System.getProperty("JMS.PORT", "8080");
+		remoteConnectionFactoryJndiName = System.getProperty("JMS.CONNECTION.FACTORY.JNDI.NAME", "jms/RemoteConnectionFactory");
+		topicJndiName = System.getProperty("JMS.TOPIC.JNDI.NAME", "jms/topic/EventTopic");
+		providerUrl = initialContextLookupProtocol + "://" + jmsHost + ":" + jmsPort;
+	}
+	
 	public void publish() {
 		try {
 			Properties props = new Properties();
-			props.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-			props.put(Context.PROVIDER_URL, "http-remoting://localhost:8081");
+			props.put(Context.INITIAL_CONTEXT_FACTORY, initialContextFactory);
+			props.put(Context.PROVIDER_URL, providerUrl);
 			InitialContext context = new InitialContext(props);
 			
 
-			javax.jms.Topic topic = (javax.jms.Topic)context.lookup("jms/topic/EventTopic");
+			javax.jms.Topic topic = (javax.jms.Topic)context.lookup(topicJndiName);
 			
-			javax.jms.ConnectionFactory connectionFactory = (javax.jms.ConnectionFactory)context.lookup("jms/RemoteConnectionFactory");
+			javax.jms.ConnectionFactory connectionFactory = (javax.jms.ConnectionFactory)context.lookup(remoteConnectionFactoryJndiName);
 
             // Create a Connection
-            Connection connection = connectionFactory.createConnection("guest", "butt");
+            Connection connection = connectionFactory.createConnection();
             connection.start();
 
             // Create a Session
