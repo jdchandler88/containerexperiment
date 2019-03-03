eb6c2fb17597c3ba56fcd008a7c3ce8286740d7e
jdchandler88 <jdchandler88@gmail.com>
2019-03-02 20:44:17 -0500

    adding vue app. it is now using vuetify

--- services/ui/src/App.vue
+++ services/ui/src/App.vue
@@ -1,56 +1,38 @@
 <template>
-  <div id="app">
-    <input v-model="message"/>
-    <button @click="publishMessage">Publish</button>
-    <button id="btn" @click="getResponse">Get Response</button>
-    <label for="btn">{{response}}</label>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import axios from 'axios';
+import HelloWorld from './components/HelloWorld'
+
 export default {
-  name: 'app',
-  data: function() {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
     return {
-      message: "Default message",
-      response: ""
-    }
-  },
-  methods: {
-    publishMessage() {
-      console.log("URL: " + this.simpleServiceUrl);
-      axios.post(this.simpleServiceUrl, this.message)
-      .then(result => {
-        console.log("SUCCESS: ", result);
-      }).catch(error => {
-        console.log("ERROR: ", error);
-      });
-    },
-    getResponse() {
-      axios.get(this.simpleServiceUrl)
-        .then(result=> {
-          this.response = result.data
-        }).catch(error=> {
-          console.log("ERROR: ", error);
-        })
-    }
-  },
-  computed: {
-    simpleServiceUrl() {
-      return process.env.VUE_APP_SIMPLE_SERVICE_HOST  + process.env.VUE_APP_SIMPLE_SERVICE_PATH;
+      //
     }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
