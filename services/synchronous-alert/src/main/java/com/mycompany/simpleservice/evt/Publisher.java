7ce0b61fe84138a3ab5bb380773da78674ddf0b2
jdchandler88 <jdchandler88@gmail.com>
2019-03-03 20:35:01 -0500

    added an asynchronous alert service. this is very stupid. it generates an alert on a schedule. this was added to docker-compose and is now a part of the enterprise

---
+++ services/synchronous-alert/src/main/java/com/mycompany/simpleservice/evt/Publisher.java
@@ -0,0 +1,118 @@
+package com.mycompany.simpleservice.evt;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+@Stateless
+public class Publisher {
+
+	private static final String DEFAULT_INITIAL_CONTEXT_FACTORY = "org.jboss.naming.remote.client.InitialContextFactory";
+	
+	private static final String DEFAULT_CONTEXT_LOOKUP_PROTOCOL = "http-remoting";
+	
+	private static final String DEFAULT_JMS_HOST = "jms";
+	
+	private static final String DEFAULT_JMS_PORT = "8080";
+	
+	private static final String DEFAULT_JMS_CONNECTION_FACTORY_JNDI_NAME = "jms/RemoteConnectionFactory";
+	
+	private static final String DEFAULT_JMS_TOPIC_JNDI_NAME = "jms/topic/EventTopic";
+	
+	private String initialContextFactory;
+
+	private String initialContextLookupProtocol;
+	
+	private String jmsHost;
+	
+	private String jmsPort;
+	
+	private String remoteConnectionFactoryJndiName;
+	
+	private String topicJndiName;
+	
+	private String providerUrl;
+	
+	@PostConstruct
+	public void init() {
+		initialContextFactory = System.getProperty("INITIAL.CONTEXT.FACTORY", DEFAULT_INITIAL_CONTEXT_FACTORY);
+		initialContextLookupProtocol = System.getProperty("CONTEXT.LOOKUP.PROTOCOL", DEFAULT_CONTEXT_LOOKUP_PROTOCOL);
+		jmsHost = System.getProperty("JMS.HOST", DEFAULT_JMS_HOST);
+		jmsPort = System.getProperty("JMS.PORT", DEFAULT_JMS_PORT);
+		remoteConnectionFactoryJndiName = System.getProperty("JMS.CONNECTION.FACTORY.JNDI.NAME", DEFAULT_JMS_CONNECTION_FACTORY_JNDI_NAME);
+		topicJndiName = System.getProperty("JMS.TOPIC.JNDI.NAME", DEFAULT_JMS_TOPIC_JNDI_NAME);
+		providerUrl = initialContextLookupProtocol + "://" + jmsHost + ":" + jmsPort;
+	}
+	
+//	@Resource(lookup = "java:/jms/topic/EventTopic")
+//	private Destination destination;
+//	
+//	@Resource(lookup = "java:/ConnectionFactory")
+//	private ConnectionFactory cxf;
+	
+	@Schedule(second="*/30", minute="*", hour="*")
+	public void publish() {
+		
+		
+		try {
+			Properties props = new Properties();
+			props.put(Context.INITIAL_CONTEXT_FACTORY, initialContextFactory);
+			props.put(Context.PROVIDER_URL, providerUrl);
+			InitialContext context = new InitialContext(props);
+			
+ 
+			javax.jms.Topic topic = (javax.jms.Topic)context.lookup(topicJndiName);
+			
+			javax.jms.ConnectionFactory connectionFactory = (javax.jms.ConnectionFactory)context.lookup(remoteConnectionFactoryJndiName);
+
+			
+			
+            // Create a Connection
+            Connection connection = connectionFactory.createConnection();
+			
+//            System.out.println("DESTINATION::::::::::::::::" + destination);
+//			System.out.println("CONNECTIONFACTORY:::::::::::" + cxf);
+			System.out.println("DESTINATION::::::::::::::::" + topic);
+			System.out.println("CONNECTIONFACTORY:::::::::::" + connectionFactory);
+//			
+//			Connection connection = cxf.createConnection();
+            connection.start();
+
+            // Create a Session
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // Create a MessageProducer from the Session to the Topic or Queue
+//            MessageProducer producer = session.createProducer(destination);
+            MessageProducer producer = session.createProducer(topic);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+            String messageText = Date.from(Instant.now()).toString();
+            // Create a messages
+            TextMessage message = session.createTextMessage(messageText);
+
+            // Tell the producer to send the message
+            System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName() + "---" + messageText);
+            producer.send(message);
+
+            // Clean up
+            session.close();
+            connection.close();
+        }
+        catch (Exception e) {
+            System.out.println("Caught: " + e);
+            e.printStackTrace();
+        }
+	}
+	
+}
